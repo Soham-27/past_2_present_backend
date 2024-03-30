1a4e8fcdeb990188cf35ddbf81b5605b54552223
Soham-27 <phadkesoham2004@gmail.com>
2024-03-31 00:49:38 +0530

    changes

--- server/models/db.js
+++ server/models/db.js
@@ -1,16 +1,21 @@
 import pg from "pg";
 const {Client}=pg;
 const db=new pg.Client({
-    user:"postgres"||process.env.USER,
-    host:"localhost"||process.env.HOST,
-    database:"past2present"||process.env.DATABASE,
-    password:"bharat"||process.env.PASSWORD,
-    port:5432||process.env.PORT,
+    // user:"postgres"||process.env.USER,
+    // host:"localhost"||process.env.HOST,
+    // database:"past2present"||process.env.DATABASE,
+    // password:"bharat"||process.env.PASSWORD,
+    // port:5432||process.env.PORT
+    user:process.env.USER,
+    host:process.env.HOST,
+    database:process.env.DATABASE,
+    password:process.env.PASSWORD,
+    port:process.env.PORT,  
 });
-if(db .connect()){
+if(db .connect()){  
     console.log("database connected!!")
 }
 else{
     console.log("error in the connecting database");
 }
-export {db};  
+export {db}; 
