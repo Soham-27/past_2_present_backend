f8edd7b422712d92fb28f697a5b7f773c7fbd621
Soham-27 <phadkesoham2004@gmail.com>
2024-04-03 10:57:47 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config({path:"../config/.env"});
 import { db } from "./models/db.js";
+import cors from 'cors';
 import { userrouter } from "./routes/user_routes.js";
 import bodyParser from "body-parser";
 import { isUserAuthenticated } from "./middleware/user_middleware.js";
 import { itemrouter } from "./routes/items_routes.js";
 import { profilerouter } from "./routes/profile_routes.js";
-app.use(cors());
+
+
  
 
 
@@ -16,11 +18,11 @@ const app=express();
 const port=process.env.PORT; 
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse U  
-
+app.use(cors());
 
 app.use("/profile",profilerouter)
 app.use("/user",userrouter);
-app.use("/items",itemrouter);
+app.use("/items",itemrouter); 
 app.get("/",async(req,res)=>{
     res.json("hello from");
     console.log("yo")
